0b955b46977dde64aa326034c3d56c77001a36cd
stvmachine <campos.esteban@gmail.com>
2018-10-19 10:47:45 +1100

    parsing basic props for the style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,39 @@
 import styled from "styled-components";
 
+const display = (backgroundColor, color) =>
+  `background-color: ${backgroundColor}; 
+   color: ${color};
+   `;
+
+const kinds = {
+  primary: display("#1FB6FF", "white"),
+  secondary: display("#5352ED", "white"),
+  cancel: display("#FF4949", "white"),
+  dark: display("#273444", "white"),
+  gray: display("#8492A6", "white")
+};
+
+const scales = {
+  small: `
+      padding: 5px 10px;
+      font-size: 14px;
+    `,
+  normal: `
+      padding: 10px 20px;
+      font-size: 16px;
+    `,
+  big: `
+      padding: 20px 30px;
+      font-size: 18px;
+    `
+};
 
 const Button = styled.button`
-    width: 200px;
-    color: white;
-    background-color: blue;
+  width: 200px;
+  ${props => (props.scale ? scales[props.scale] : scales.normal)};
+  ${props =>
+    props.kind ? kinds[props.kind] : kinds.primary};
 `;
+
 
 export default Button;
